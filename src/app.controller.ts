5f75cee786ef6f62cac3291b4ada8dad1480bd2f
Felipe Esaki <feliperoman1703@hotmail.com>
2025-08-03 16:33:05 -0300

    feat: adicionando método post e get

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './database/prisma.service';
 
-@Controller()
+@Controller('products')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private prisma: PrismaService
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  @Post('headphones')
+    async addProduct() {
+      const product = await this.prisma.products.create({
+        data: {
+          imgUrl: "/images/tecnology/headphone4",
+          title: "soundcore by Anker Q20i Fone de Ouvido Bluetooth",
+          description: "Headphone com Cancelamento de Ruído Híbrido, 40H de Reprodução com ANC, Áudio Hi-Res, Graves Potentes, Personalização no App, Modo Transparência, Preto",
+          price: 23000,
+          category: "tecnology",
+          subCategory: "headphones",
+          stock: 5
+        }
+      })
+      return product
+    }
+
+  @Get('headphones')
+    async retrieveAll() {
+      const products = await this.prisma.products.findMany({
+        where: {
+          subCategory: 'headphones'
+        }
+      })
+      return products
+    }
 }
- 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { OiService } from './oi/oi.service';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, OiService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
